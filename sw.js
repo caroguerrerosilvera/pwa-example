deae1fdabcde99b9b69e355f7b7ebcabf2ad1057
Caro Guerrero <caroguerrero@MacBook-Air-de-Caro.local>
2022-01-07 15:17:04 -0500

    add installation feature

--- sw.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("beforeinstallprompt", function (e) {
-  // log the platforms provided as options in an install prompt
-  console.log(e.platforms); // e.g., ["web", "android", "windows"]
-  e.userChoice.then(function (choiceResult) {
-    console.log(choiceResult.outcome); // either "accepted" or "dismissed"
-  }, handleError);
-});
